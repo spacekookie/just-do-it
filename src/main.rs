0ca3b89991338dfb4843f5fec94971638e542bfc
Katharina Fey <kookie@spacekookie.de>
2018-04-18 16:28:54 +0200

    First useful version (to me :P)

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate clap;
+extern crate colored;
+extern crate num_cpus;
 extern crate question;
+
 #[macro_use]
 extern crate human_panic;
-use clap::{App, Arg, SubCommand};
+
+use clap::{App, AppSettings, Arg, SubCommand};
 use question::{Answer, Question};
 use std::process::{self, Command};
+use colored::*;
 
 const DELETE_QUESTION: &'static str =
     "Are you sure you want to destroy 🔥 (and murder 🔪) this container and all of it's happy friends? ";
@@ -13,6 +18,7 @@ fn main() {
     setup_panic!();
 
     let m = App::new("doit")
+        .setting(AppSettings::SubcommandRequired)
         .about("It works for me, okay?")
         .subcommand(
             SubCommand::with_name("new")
@@ -23,15 +29,41 @@ fn main() {
             SubCommand::with_name("destroy")
                 .about("Murder existing containers & their families in cold bits...")
                 .arg(Arg::with_name("name").takes_value(true).required(true)),
+        )
+        .subcommand(
+            SubCommand::with_name("work")
+                .about("Start and attach a container to work with it")
+                .arg(Arg::with_name("name").takes_value(true).required(true))
+                .arg(
+                    Arg::with_name("shutdown")
+                        .default_value("no")
+                        .possible_values(&["yes", "no", "hell no"])
+                        .help("Shutdown this container after working on it"),
+                ),
         );
 
     match m.get_matches().subcommand() {
         ("new", Some(m)) => {
             let name = m.value_of("name").unwrap();
+            println!(
+                "Creating a new container from scratch, with my bare {} cores!",
+                num_cpus::get()
+            );
             docker_create(name);
+            docker_start(name);
             docker_base_install(name);
             println!("✨✨✨ Container created and ready: {} ✨✨✨", name);
         }
+        ("work", Some(m)) => {
+            let name = m.value_of("name").unwrap();
+            println!(
+                "Look at you busy bee, let's get you ready working on ✨ {} ✨",
+                name
+            );
+            docker_start(name);
+            docker_attach(name);
+        }
+
         ("destroy", Some(m)) => {
             let name = m.value_of("name").unwrap();
             let answer = Question::new(DELETE_QUESTION)
@@ -43,14 +75,13 @@ fn main() {
                 process::exit(2);
             }
 
+            docker_stop(name);
             docker_delete(name);
 
             println!("Deleted container {} 🔥🔥🔥", name);
         }
         _ => unreachable!(),
     }
-
-    println!("Hello, world!");
 }
 
 fn docker_create(name: &str) {
@@ -69,7 +100,38 @@ fn docker_create(name: &str) {
         .expect(&format!("docker create failed for {:?}", &args));
 
     if !cmd.wait().unwrap().success() {
-        eprintln!("Failed to run create command!");
+        eprintln!("{}", "Failed to run create command!".red());
+        process::exit(2);
+    }
+}
+
+/// Only starts IF ABSOLUTELY NECESSARY
+///
+/// Okay I'm being more dramatic than neccessary.
+///
+/// O R    A M     I
+fn docker_start(name: &str) {
+    let args = ["start", name];
+    let mut cmd = Command::new("docker")
+        .args(&args)
+        .spawn()
+        .expect(&format!("docker start failed for {:?}", &args));
+
+    if !cmd.wait().unwrap().success() {
+        eprintln!("{}", "Failed to run start command!".red());
+        process::exit(2);
+    }
+}
+
+fn docker_stop(name: &str) {
+    let args = ["stop", name];
+    let mut cmd = Command::new("docker")
+        .args(&args)
+        .spawn()
+        .expect(&format!("docker stop failed for {:?}", &args));
+
+    if !cmd.wait().unwrap().success() {
+        eprintln!("{}", "Failed to run stop command!".red());
         process::exit(2);
     }
 }
@@ -82,7 +144,7 @@ fn docker_delete(name: &str) {
         .expect(&format!("docker delete failed for {:?}", &args));
 
     if !cmd.wait().unwrap().success() {
-        eprintln!("Failed to run delete command!");
+        eprintln!("{}", "Failed to run delete command!".red());
         process::exit(2);
     }
 }
@@ -114,7 +176,21 @@ fn docker_base_install(name: &str) {
         .expect(&format!("docker base install failed for {:?}", args));
 
     if !cmd.wait().unwrap().success() {
-        eprintln!("Failed to run install command!");
+        eprintln!("{}", "Failed to run install command!".red());
+        process::exit(2);
+    }
+}
+
+/// This doesn't return
+fn docker_attach(name: &str) {
+    let args = ["attach", name];
+    let mut cmd = Command::new("docker")
+        .args(&args)
+        .spawn()
+        .expect(&format!("docker start failed for {:?}", &args));
+
+    if !cmd.wait().unwrap().success() {
+        eprintln!("{}", "Failed to run start command!".red());
         process::exit(2);
     }
 }
